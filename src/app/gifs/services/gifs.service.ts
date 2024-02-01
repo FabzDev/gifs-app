400d47cdc868a50c8169b81db7ac20e472add217
Fabio <fabioescobarardila@hotmail.com>
2024-02-01 16:54:17 -0500

    Creando una interface para la respuesta json usando PASTE JSON AS CODE de quicktype.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 const GIFS_API_KEY = 'ybboGP7QGwVdJvwi4E1x2Yc22tVgYqhS'
 const gifsUrlService = 'https://api.giphy.com/v1/gifs'
@@ -7,6 +8,7 @@ const gifsUrlService = 'https://api.giphy.com/v1/gifs'
 @Injectable({providedIn: 'root'})
 export class GifsService {
   private _tagHistory: string[] =[]
+  public gifList: Gif[] = []
 
   constructor(private http: HttpClient) { }
 
@@ -35,16 +37,17 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag)
 
-    this.http.get( `${gifsUrlService}/search`, { params: httpParams } )
+    this.http.get<SearchResponse>( `${gifsUrlService}/search`, { params: httpParams } )
     .subscribe( resp => {
-      console.log(resp);
+      this.gifList = resp.data
+
+      console.log(this.gifList);
     }) // { params: httpParams } podria ser solo { params } si httpParams se llamara params
 
     // fetch('https://api.giphy.com/v1/gifs/search?api_key=ybboGP7QGwVdJvwi4E1x2Yc22tVgYqhS&q=palestine&limit=10')
     // .then(resp => resp.json())
     // .then(data => console.log(data)
     // )
-
   }
 
 }
