0c7d5b62662427ecd388be3c596bc8765ca39899
Fabio <fabioescobarardila@hotmail.com>
2024-02-01 11:24:34 -0500

    @ViewChild: Tomas un elemento html para usarlo en la lógica JS

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
-  templateUrl: 'search-box.component.html'
+  templateUrl: 'search-box.component.html',
 })
+export class SearchBoxComponent {
+  constructor(public gifsService: GifsService) {}
 
-export class SearchBoxComponent{
-  constructor() { }
+  @ViewChild('searchInput')
+  public searchInput!: ElementRef<HTMLInputElement>;
 
+  searchTag(): void {
+    const newTag = this.searchInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.searchInput.nativeElement.value = ''
+  }
 }
