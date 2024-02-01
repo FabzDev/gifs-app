1ba323d07957f7c97cc93fb6b5d970f51a29f0a2
Fabio <fabioescobarardila@hotmail.com>
2024-02-01 16:21:54 -0500

    HttpClientModule: es un paquete muy fuerte de angular para trabajar con observables y programación reactiva. HttpClient: es un servicio muy util para hacer peticiones http. HttpParams: es un servicio utilizado para encapsular los parametros/queries de la petición.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+const GIFS_API_KEY = 'ybboGP7QGwVdJvwi4E1x2Yc22tVgYqhS'
+const gifsUrlService = 'https://api.giphy.com/v1/gifs'
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
-
   private _tagHistory: string[] =[]
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagHistory(){
     return [...this._tagHistory]
@@ -23,9 +26,24 @@ export class GifsService {
     this._tagHistory = this._tagHistory.splice(0,10)
   }
 
-  searchTag( tag: string ):void {
+  searchTag( tag: string ): void {
     if(tag=='') return
     this.orderHistory(tag)
+
+    const httpParams = new HttpParams()
+      .set('api_key', GIFS_API_KEY)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get( `${gifsUrlService}/search`, { params: httpParams } )
+    .subscribe( resp => {
+      console.log(resp);
+    }) // { params: httpParams } podria ser solo { params } si httpParams se llamara params
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=ybboGP7QGwVdJvwi4E1x2Yc22tVgYqhS&q=palestine&limit=10')
+    // .then(resp => resp.json())
+    // .then(data => console.log(data)
+    // )
 
   }
 
