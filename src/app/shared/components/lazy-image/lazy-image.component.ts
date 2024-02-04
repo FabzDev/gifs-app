de65b7eb2da09bc0ba7003b3435549ac409d746a
Fabio <fabioescobarardila@hotmail.com>
2024-02-03 22:08:16 -0500

    FadeIn aplicado a las imagenes

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'shared-lazy-image',
@@ -19,8 +20,7 @@ export class LazyImageComponent implements OnInit{
   }
 
   imgLoaded(){
-    console.log('Image loaded');
-    this.hasLoaded = true
+      this.hasLoaded = true
   }
 
 
